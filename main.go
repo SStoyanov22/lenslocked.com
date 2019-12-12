fb9890836bac05f547eafde82c3a5ff3bac6a43b
SStoyanov22 <SStoyanov22@gmail.com>
2019-12-12 08:43:22 +0200

    Added a 404 page

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my super awsome site!</h1>")
 	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<a href=\"support@lenslocked.com\">support@lenslocked.com</a/")
+		fmt.Fprint(w, "<a href=\"support@lenslocked.com\">support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Page not found</h1><p>Please contact us if you keep being sent to an invalid page</p>")
 	}
 
 }
