2008332c2c6f09c70c1137dcc2263279cb60a173
SStoyanov22 <SStoyanov22@gmail.com>
2019-12-12 06:46:37 +0200

    Updating content-header to text/html

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Someone visited our page", r.URL)
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type ", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my super awsome site!</h1>")
 }
 func main() {
