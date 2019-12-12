743c43b34fb7b136b6bd56850c761df57b592987
SStoyanov22 <SStoyanov22@gmail.com>
2019-12-12 06:43:16 +0200

    Added content-type header

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Someone visited our page", r.URL)
+	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprint(w, "<h1>Welcome to my super awsome site!</h1>")
 }
-
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe("localhost:3000", nil)
