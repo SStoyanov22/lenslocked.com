9002be13ee0d112b8658436a12eddbc198deb8e2
SStoyanov22 <SStoyanov22@gmail.com>
2019-12-17 08:01:21 +0200

    Adding home template

--- main.go
+++ main.go
@@ -1,47 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"github.com/julienschmidt/httprouter"
+	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-}
+var homeTemplate *template.Template
 
-func handlerFunc(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awsome site!</h1>")
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>To get in touch, please send and email to <a href = \"support@lenslocked.com\"</h1>")
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ Page</h1>")
-}
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	r := httprouter.New()
-	r.GET("/", homeHandler)
-	r.GET("/contact", contactHandler)
-	r.GET("/faq", faqHandler)
-	r.NotFound = http.HandlerFunc(notFoundHandler)
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
 	http.ListenAndServe(":8000", r)
 }
