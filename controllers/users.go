06e2f413a8da3ee23bf49f376da3055a6afbe439
SStoyanov22 <SStoyanov22@gmail.com>
2020-01-04 11:37:42 +0200

    Created a signup action

--- controllers/users.go
+++ controllers/users.go
@@ -3,6 +3,7 @@ package controllers
 import (
 "github.com/SStoyanov22/lenslocked.com/views"
 "net/http"
+"fmt"
 )
 
 //used to create new users controller
@@ -22,8 +23,18 @@ func New(u *Users, w http.ResponseWriter, r *http.Request){
 
 }
 
+
+//GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request){
 	if err := u.NewView.Render(w,nil); err!= nil {
 		panic(err)
 	}
+}
+
+//Create is used to process the signup form when a user submits it.
+//This is used to create a new users account
+//
+//POST /signup
+func (u *Users) Create (w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w,"This is temporary response.")
 }
