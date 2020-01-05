0bebe83582d5cf937ede03bcb820c5fba8d550d8
SStoyanov22 <SStoyanov22@gmail.com>
2020-01-05 06:28:03 +0200

    Applying a helper in order to not repeat form parsing

--- controllers/users.go
+++ controllers/users.go
@@ -41,19 +41,9 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request){
 //
 //POST /signup
 func (u *Users) Create (w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm()	; err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err:= dec.Decode(&form, r.PostForm); err!=nil{
+	if err := parseForm(r,&form); err!=nil{
 		panic(err)
 	}
-
 	fmt.Fprintln(w, form)
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
-	fmt.Fprintln(w, r.PostFormValue("email"))
-	fmt.Fprintln(w,"This is temporary response.")
 }
